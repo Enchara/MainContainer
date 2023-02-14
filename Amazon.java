2a6c1a0b82a9efe9add7214718dd04e8a6f30b74
Enchara <ranjithamahesh719@gmail.com>
2023-02-14 12:56:29 +0530

    push

---
+++ Amazon.java
@@ -0,0 +1,22 @@
+class Amazon_a1
+{
+void search()
+{
+System.out.println("searching by text");
+}
+}
+class Amazon_a2
+{
+void search()
+{
+System.out.println("searching by text,images,voice");
+}
+}
+class Amazon
+{
+public static void main(String[] args)
+{
+ Amazon_a2 W2=new Amazon_a2();
+W2.search();
+}
+}
